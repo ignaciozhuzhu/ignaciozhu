067e8fb378ba67f9d1c816bbc555c0291c9b250a
龙鸿轩 <119414860@qq.com>
2016-01-06 11:02:05 +0800

    1
    1

---
+++ NUnitTest/NunitTestTest/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using NUnitTest;
+
+namespace NunitTestTest
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void TestAdd()
+        {
+            Calculator cal = new Calculator();
+            int expected = 5;
+            int actual = cal.Add(2, 3);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestDivide()
+        {
+            Calculator cal = new Calculator();
+            int expected = 5;
+            int actual = cal.Divide(25, 0);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
